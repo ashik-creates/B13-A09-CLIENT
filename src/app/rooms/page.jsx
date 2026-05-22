64f9c4aa497d92e19e8ab418643d80d06081c450
ashikur rahman <ashikorrahman8900@gmail.com>
2026-05-22 21:16:40 +0600

    added search functionality in rooms page

--- src/app/rooms/page.jsx
+++ src/app/rooms/page.jsx
@@ -1,24 +1,36 @@
 import RoomCard from "@/components/shared/RoomCard";
+import RoomSearchCard from "@/ui/RoomSearchCard";
 import React from "react";
 
-const AllRoomsPage = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/rooms`);
-  const rooms = await res.json();
+const AllRoomsPage = async ({ searchParams }) => {
+  const params = await searchParams;
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/rooms?search=${params?.search}&amenities=${params?.amenities}&min=${params?.min}&max=${params?.max}`,
+  );
 
-  if(rooms.length === 0){
-    return <p>No room available</p>
-  }
+  const rooms = await res.json();
 
   return (
     <div className="bg-[#FFF7D6] py-10">
       <div className="container mx-auto">
-        <div className="border-b-2 border-dashed border-gray-300 pb-5">
-          <h1 className="text-3xl font-bold text-gray-800 text-center">All Rooms</h1>
+        <div className="border-b-2 border-dashed border-gray-300 pb-5 ">
+          <h1 className="text-3xl font-bold text-gray-800 text-center">
+            All Rooms
+          </h1>
         </div>
-        <div className="grid grid-cols-3 gap-10 mt-5">
-          {rooms.map((room) => (
-            <RoomCard key={room._id} room={room}></RoomCard>
-          ))}
+        <div className="grid grid-cols-12 gap-3 mt-3 items-start">
+          <div className="grid grid-cols-3 col-span-9 gap-10 items-start">
+            {rooms.length === 0 ? (
+              <p>No room available</p>
+            ) : (
+              rooms.map((room) => (
+                <RoomCard key={room._id} room={room}></RoomCard>
+              ))
+            )}
+          </div>
+          <div className="col-span-3">
+            <RoomSearchCard></RoomSearchCard>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/ui/RoomSearchCard.jsx
@@ -0,0 +1,168 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { Input, Checkbox, CheckboxGroup, Button, Label } from "@heroui/react";
+import { FaCheck, FaSearch } from "react-icons/fa";
+import { useRouter } from "next/navigation";
+
+const RoomSearchCard = () => {
+  const [search, setSearch] = useState("");
+  const [amenities, setAmenities] = useState([]);
+  const [min, setMin] = useState("");
+  const [max, setMax] = useState("");
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const params = new URLSearchParams();
+
+    if (search) {
+      params.set("search", search)
+    };
+    if (amenities.length > 0){
+      params.set("amenities", amenities.join(','))
+    } ;
+    if (min) {
+      params.set("min", min)
+    };
+    if (max) {
+      params.set("max", max)
+    };
+
+    const paramString = params.toString()
+    if(paramString){
+      router.push(`/rooms?${paramString}`);
+    }
+    else{
+      router.push("/rooms")
+    }
+    
+  }, [search, amenities, min, max]);
+
+  const handleReset = () => {
+    setSearch("");
+    setAmenities([]);
+    setMin("");
+    setMax("");
+  };
+
+  return (
+    <div className="bg-[#f0df9c] border border-gray-200 rounded-3xl p-6 shadow-sm space-y-6">
+      <div>
+        <h2 className="text-2xl font-bold text-gray-800">
+          Find Your Study Space
+        </h2>
+
+        <p className="text-gray-500 mt-1">
+          Search rooms and filter by amenities or hourly rate.
+        </p>
+      </div>
+
+      <div className="relative">
+        <Input
+          aria-label="search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Search by room name..."
+          className="w-full"
+        />
+
+        <FaSearch className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400" />
+      </div>
+
+      <div>
+        <p className="text-base font-semibold mb-3">Amenities</p>
+        <CheckboxGroup
+          value={amenities}
+          onChange={(value) => setAmenities(value)}
+        >
+          <div className="space-y-4 text-sm">
+            {[
+              "Whiteboard",
+              "Projector",
+              "Wi-Fi",
+              "Power Outlets",
+              "Quiet Zone",
+              "Air Conditioning",
+            ].map((item) => {
+              return (
+                <label
+                  key={item}
+                  htmlFor={`amenity-${item}`}
+                  className={`flex items-center gap-2.5 px-4 py-3 rounded-full border text-sm transition-all duration-200 cursor-pointer `}
+                >
+                  <Checkbox
+                    id={`amenity-${item}`}
+                    name="amenities"
+                    value={item}
+                  >
+                    <Checkbox.Control>
+                      <Checkbox.Indicator>
+                        {({ isSelected }) => (
+                          <div
+                            className={`
+                                      flex items-center justify-center 
+                                      w-5 h-5 rounded-full 
+                                      ${
+                                        isSelected
+                                          ? "bg-[#06B6D4] border-[#06B6D4] scale-110"
+                                          : "bg-transparent border-gray-400"
+                                      }
+                                      `}
+                          >
+                            {isSelected && (
+                              <FaCheck className="text-white text-[10px]" />
+                            )}
+                          </div>
+                        )}
+                      </Checkbox.Indicator>
+                    </Checkbox.Control>
+                  </Checkbox>
+                  <Checkbox.Content>
+                    <Label htmlFor={`amenity-${item}`}>{item}</Label>
+                  </Checkbox.Content>
+                </label>
+              );
+            })}
+          </div>
+        </CheckboxGroup>
+      </div>
+
+      <div>
+        <Label className="text-sm font-semibold mb-3 block">
+          Hourly Rate Range
+        </Label>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <Input
+            value={min}
+            onChange={(e) => setMin(e.target.value)}
+            type="number"
+            label="Minimum Hourly Rate"
+            placeholder="Min"
+          />
+
+          <Input
+            value={max}
+            onChange={(e) => setMax(e.target.value)}
+            type="number"
+            label="Maximum Hourly Rate"
+            placeholder="Max"
+          />
+        </div>
+      </div>
+
+      <div className="flex items-center gap-3 pt-2">
+        <Button
+          onClick={handleReset}
+          variant="outline"
+          className={"bg-white border border-[#FACC15]"}
+        >
+          Reset
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default RoomSearchCard;
